3e19f4c224fa3b045f557ee28b987dd7abe158e3
Renata Fernandes de Franca Carneiro <renatactba@hotmail.com>
2023-01-16 17:54:09 -0300

    variaveis

--- Meu primeiro projeto Java/src/MinhaClasse.java
+++ Meu primeiro projeto Java/src/MinhaClasse.java
@@ -3,6 +3,14 @@ public class MinhaClasse{
  public static void main(String[] args) {
 
    System.out.print ("Olá")  ; 
+    //declarando variavel
+String  Nome = "Renata";
+int Ano = 2022; 
+
+
+//alterando variavel
+
+Nome = "Joao";
  }
 
 }
