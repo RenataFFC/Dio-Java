1139c97df35ef08dee394a09a42d9ef6e07aa25f
Renata Fernandes de Franca Carneiro <renatactba@hotmail.com>
2023-02-04 19:26:00 -0300

    Operador Ternario

---
+++ Meu primeiro projeto Java/src/operadorternario.java
@@ -0,0 +1,12 @@
+public class operadorternario {
+    public static void main(String[] args) {
+   int a, b;
+a = 6;
+b = 6;
+
+String resultado = a==b ? "verdadeiro":"false";
+
+System.out.println(resultado);
+        
+    }
+}
