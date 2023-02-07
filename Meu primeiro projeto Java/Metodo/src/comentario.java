06e40fb1ad754d73c4a9e256b8e9b324cb5d159c
Renata Fernandes de Franca Carneiro <renatactba@hotmail.com>
2023-02-07 17:53:42 -0300

    comentarios

---
+++ Meu primeiro projeto Java/Metodo/src/comentario.java
@@ -0,0 +1,7 @@
+public class comentario {
+    public static void main(String[] args) {
+        // Olá , sou um comentario de uma linha
+/*Olá , sou um comentario multline com mais de uma linha */
+/** olá sou um comentario no estilo de documentação */
+    }
+}
