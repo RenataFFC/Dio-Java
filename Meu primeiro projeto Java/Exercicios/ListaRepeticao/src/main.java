0243c4cff8f475bad74076cd66787766d2925b82
Renata Fernandes de Franca Carneiro <renatactba@hotmail.com>
2023-02-22 22:21:39 -0300

    Mensagem

--- Meu primeiro projeto Java/Exercicios/ListaRepeticao/src/main.java
+++ Meu primeiro projeto Java/Exercicios/ListaRepeticao/src/main.java
@@ -1,6 +1,7 @@
 public class main {
 
     public static void main(String[] args) {
+//Calculadora
 
         System.out.println("Calculadora");
         Calculadora.soma(10, 20);
@@ -8,6 +9,14 @@ public class main {
         Calculadora.multiplicao(10, 20);
         Calculadora.divisao(10, 20);
 
-    }
 
+//Obter Mensagem
+
+        System.out.println("Exercício mensagem");
+        Mensagem.obterMensagem(5);
+        Mensagem.obterMensagem(3);
+        Mensagem.obterMensagem(21);
+
+
+    }
 }
