e41d5c94d822ebd67155076f5be099995a84e268
Renata Fernandes de Franca Carneiro <renatactba@hotmail.com>
2023-03-03 15:10:17 -0300

    criando objeto java

--- Meu primeiro projeto Java/src/poo/carro.java
+++ Meu primeiro projeto Java/src/poo/carro.java
@@ -1,11 +1,49 @@
 package poo;
 
-public class carro {
+/**
+ * Classe de exemplo para o exercício da Aula 2 de Orientação a Objetos.
+ */
+class Carro {
 
-public static void ligar() {
     String cor;
-   String modelo;
-  int capacidadeTanque;
-}
-   
+    String modelo;
+    int capacidadeTanque;
+
+    Carro() {
+
+    }
+
+    Carro(String cor, String modelo, int capacidadeTanque) {
+        this.cor = cor;
+        this.modelo = modelo;
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    String getCor() {
+        return cor;
+    }
+
+    void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    String getModelo() {
+        return modelo;
+    }
+
+    void setCapacidadeTanque(int capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    int getCapacidadeTanque() {
+        return capacidadeTanque;
+    }
+
+    double totalValorTanque(double valorCombustivel) {
+        return capacidadeTanque * valorCombustivel;
+    }
 }
